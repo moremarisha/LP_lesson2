1a49ee67dc2ecd9d6ae6c8653f1b69f890cb59c4
moremarisha <m.nagov@gmail.com>
2018-05-25 14:26:51 +0300

    Delete bot_constants.py

--- bot_constants.py
+++
@@ -1,2 +0,0 @@
-bot_key = '514216029:AAGrjbrD5N7NvsWwcWGcTfEcelwF_xWy71s'
-bot_proxy = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username':'learn', 'password':'python'}}
